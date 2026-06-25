a5f386161d0880ca9640219c9ef6b36f0851234e
Luiz Palma <luizpalma16@gmail.com>
2026-06-24 23:45:43 -0300

    feat(api): expose addressLookup via GraphQL (Hot Chocolate)

---
+++ src/Financarias.Api/GraphQL/Query.cs
@@ -0,0 +1,15 @@
+﻿using Financarias.Application.Addresses;
+
+namespace Financarias.Api.GraphQL;
+
+public class Query
+{
+    [GraphQLName("addressLookup")]
+    public async Task<AddressLookupResult?> AddressLookupAsync(
+        string cep,
+        IAddressLookupProvider addressLookupProvider,
+        CancellationToken cancellationToken)
+    {
+        return await addressLookupProvider.FindAddressAsync(cep, cancellationToken);
+    }
+}
--- src/Financarias.Api/Program.cs
+++ src/Financarias.Api/Program.cs
@@ -1,3 +1,6 @@
+using Financarias.Api.GraphQL;
+using Financarias.Integrations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddIntegrations(builder.Configuration);
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
 
 var app = builder.Build();
 
@@ -19,5 +28,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGraphQL();
 
 app.Run();
